ad9154c1158b5c462d5e0eec8a543fa025c8e352
Anshuman Singh <www.anshu2000@gmail.com>
2023-03-18 14:16:48 +0530

    added config to client

--- client/app.config.ts
+++ client/app.config.ts
@@ -1,16 +1,30 @@
-module.exports = {
+interface ExpoConfig {
+  name: string,
+  version: string,
+  extra: {
+    apiUrl: string,
+    hand: string
+  }
+}
+
+const config: ExpoConfig = {
   name: 'MyApp',
   version: '1.0.0',
   extra: {
-    eas: {
-      projectId: "bf85755d-4e0f-43c5-a3d0-af4a658e2dab"
-    },
     apiUrl: process.env.API_URL || 'http://192.168.43.154:3000',
-    android: {
-      package: "mrsingh",
-    },
-  },
+    hand: "Handis this"
+  }
 };
+
+export default config;
+
+// module.exports = {
+//   name: 'MyApp',
+//   version: '1.0.0',
+//   extra: {
+//     apiUrl: process.env.API_URL || 'http://192.168.43.154:3000',
+//   },
+// };
 
 
   // url 1 = http://192.168.43.154:3000
